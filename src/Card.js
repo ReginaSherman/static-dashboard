b72aa402bf630eaf47c23b54ff8a4faa81123d59
Reg-Sherman <reginaxsherman@gmail.com>
2022-01-11 08:50:37 -0600

    updated increasing/decreasing

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Card.css'
 
+
 function Card (props) {
     
     return (
@@ -8,7 +9,7 @@ function Card (props) {
         <h3 className='initials'>{ props.initials }</h3>
         <h1 className='name'>{ props.name }</h1>
         <h2 className='handle'>{ props.handle }</h2>
-        <h3 className='trend'>{ props.trend }</h3>
+        <h3 className='trend' style={{color: props.increasing ? 'green' : 'red'}}>{ props.trend }</h3>
         <h3 className='increasing'>{ props.increasing }</h3>
         <h3 className='numOfSales'>{ props.numOfSales } sales</h3>
         </section>
